cf7aba9b48a930b108e6238be10fb3cc863f3556
Daniel Scarpim <danielscarpim@gmail.com>
2019-08-08 16:33:38 +0200

    Display data on document list

--- src/App.js
+++ src/App.js
@@ -3,10 +3,33 @@ import List from './List';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      documents: [],
+      user: {}
+    }
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:3000/data/documents')
+      .then(results => results.json())
+      .then((data) => {
+        this.setState({documents: data.documents});
+        console.log(this.state);
+      })
+
+    fetch('http://localhost:3000/data/info')
+      .then(results => results.json())
+      .then((data) => {
+        this.setState({user: data.body.user });
+      })
+  }
 
   render() {
     return (<div className="App">
-              <List />
+              <List documents={this.state.documents} />
             </div>)
   }
 }
--- src/List.js
+++ src/List.js
@@ -1,31 +1,17 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 
 class List extends Component {
-  constructor() {
-    super();
-    this.state = {
-      documents: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3000/data/documents')
-      .then(results => {
-        return results.json();
-      }).then((data) => {
-        console.log('data: ', data);
-      })
-
-    fetch('http://localhost:3000/data/info')
-      .then(results => {
-        return results.json();
-      }).then((data) => {
-        console.log('data: ', data);
-      })
-  }
-
   render() {
-    return 'test'
+    const documents = this.props.documents.map((document) =>
+      <li key={document.name + document.date}>
+        {document.name}
+        {document.date}
+      </li>
+    );
+
+    return (<ul>
+              {documents}
+            </ul>)
   }
 }
 
