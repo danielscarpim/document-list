76da5161eeacc3530dede6f71168798279d49c81
Daniel Scarpim <danielscarpim@gmail.com>
2019-08-08 14:45:51 +0200

    Conver App to class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import List from './List';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <List />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (<div className="App">
+              <List />
+            </div>)
+  }
 }
 
 export default App;
