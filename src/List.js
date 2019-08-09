9aff64bef162e4815052293d8e8f7e4b3387406d
Daniel Scarpim <danielscarpim@gmail.com>
2019-08-09 14:42:36 +0200

    Format date

--- src/List.js
+++ src/List.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 
 class List extends Component {
+
+  displayDate(date) {
+    const d = new Date(date);
+    return `${('0' + d.getDate()).slice(-2)}-${('0' + d.getDate()).slice(-2)}-${d.getFullYear()}`;
+  }
+
   render() {
-    const documents = this.props.documents.map((document, index) =>
-      <li className="document-list__item" key={index}>
-        {document.name}
-        {document.date}
-      </li>
-    );
+    const documents = this.props.documents.map((document, index) => {
+      const displayDate = this.displayDate(document.date);
+
+      return (
+        <li className="document-list__item" key={index}>
+          {document.name}
+          {displayDate}
+        </li>
+      )
+    });
 
     return (<ul className="document-list">
               {documents}
