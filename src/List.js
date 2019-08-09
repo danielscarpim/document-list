aa4907202e51945e2001d05e6c08ead4df2ecd28
Daniel Scarpim <danielscarpim@gmail.com>
2019-08-09 12:49:58 +0200

    Add basic list styles

--- src/List.js
+++ src/List.js
@@ -3,13 +3,13 @@ import React, {Component} from 'react';
 class List extends Component {
   render() {
     const documents = this.props.documents.map((document) =>
-      <li key={document.name + document.date}>
+      <li className="document-list__item" key={document.name + document.date}>
         {document.name}
         {document.date}
       </li>
     );
 
-    return (<ul>
+    return (<ul className="document-list">
               {documents}
             </ul>)
   }
